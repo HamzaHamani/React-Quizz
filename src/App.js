ec21f423e9bef74ab849249df5b26db13eb46d52
HamzaHamani <hamzahamani2004@gmail.com>
2023-10-10 02:16:51 +0100

    USEREDUCER adding use reducer for error handling and state managment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,54 @@
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
 function App() {
-  return <div className="App"></div>;
+  const initialState = {
+    questions: [],
+
+    //loding erro  ready active finished
+    status: "loading",
+  };
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case "dataRecieved":
+        return {
+          ...state,
+          questions: action.payload,
+          status: "ready",
+        };
+      case "dataFailed":
+        return {
+          ...state,
+          status: "error",
+        };
+      default:
+        throw new Error("action uknown");
+    }
+  }
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    async function Fetch() {
+      try {
+        const res = await fetch("http://localhost:9000/questions");
+        const data = await res.json();
+        dispatch({ type: "dataRecieved", payload: data });
+      } catch (err) {
+        dispatch({ type: "dataFailed" });
+      }
+    }
+    Fetch();
+  }, []);
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question</p>
+      </Main>
+    </div>
+  );
 }
 
 export default App;
