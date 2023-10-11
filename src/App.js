f4fbec7b4f32dcbd5da8ca497a0b68bf0a3b827a
HamzaHamani <hamzahamani2004@gmail.com>
2023-10-11 02:30:59 +0100

    added info screens error, loading, and start screens

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Error from "./Error";
+import Loader from "./Loader";
+import StartScreen from "./StartScreen";
 function App() {
   const initialState = {
     questions: [],
@@ -26,8 +29,9 @@ function App() {
         throw new Error("action uknown");
     }
   }
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
 
+  const numQuestion = questions.length;
   useEffect(() => {
     async function Fetch() {
       try {
@@ -44,8 +48,9 @@ function App() {
     <div className="app">
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Question</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestion={numQuestion} />}
       </Main>
     </div>
   );
