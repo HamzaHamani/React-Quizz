6c824e4d417c9d3f2a591f99728ecfbd0432ddff
HamzaHamani <hamzahamani2004@gmail.com>
2023-10-12 19:09:34 +0100

    fixed button issue

--- src/components/Options.js
+++ src/components/Options.js
@@ -5,8 +5,15 @@ function Options({ question, answer, dispatch }) {
     <div className="options">
       {question.options.map((option, index) => (
         <button
+          disabled={answer !== null}
           key={option}
-          className={`btn btn-option ${answer === index ? "answer" : ""}`}
+          className={`btn btn-option ${answer === index ? "answer" : ""} ${
+            hasAnswered
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
           onClick={() => {
             dispatch({ type: "newAnswer", payload: index });
           }}
